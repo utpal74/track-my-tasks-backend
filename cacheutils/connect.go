214671e21b383fcd467c825b784d2ce796ff7bce
UTPAL KUMAR <utpal.kumar74@gmail.com>
2025-08-04 14:34:41 +0530

    sort out redis

--- cacheutils/connect.go
+++ cacheutils/connect.go
@@ -12,55 +12,59 @@ import (
 	"go.uber.org/zap"
 )
 
+var (
+	client *redis.Client
+	env, redisURL, serverName string
+)
+
+// Connect returns a vlid connection with redis instance
 func Connect(ctx context.Context) (*redis.Client, error) {
 	logger := logger.FromCtx(ctx)
-	logger.Info("attmepting connection with redis")
+	redisURL, serverName = parseRedisConfig(ctx)
+	
+	// redis client setup (recommended in local development setup)
+	client = redis.NewClient(&redis.Options{
+		Addr: redisURL,
+		DB:   0,
+	})
 
-	redisURL := os.Getenv("REDIS_URL")
-	if redisURL == "" {
-		return nil, fmt.Errorf("REDIS_URL environment variable is not set")
-	}
-
-	env := os.Getenv("ENV") // "local" or "prod"
-	var client *redis.Client
-
-	serverName := os.Getenv("REDIS_TLS_SERVER_NAME")
-	if serverName == "" {
-		return nil, fmt.Errorf("REDIS_TLS_SERVER_NAME is not set")
-	}
-
-	if env != "production" {
-		// Docker defaults: no TLS, host might be "localhost" or Docker bridge IP
-		client = redis.NewClient(&redis.Options{
-			Addr: redisURL,
-			DB:   0,
-		})
-	} else {
+	// redis client setup overriden in prod
+	if os.Getenv("ENV") == "production" {
+		logger.Info("Attempt redis connection in production mode")
 		opt, err := redis.ParseURL(redisURL)
-		common.FailOnError(ctx, fmt.Sprintf("failed to parse Redis URL: %w", err), err)
+		common.FailOnError(ctx, fmt.Sprintf("failed to parse Redis URL: %v", err), err)
 
 		opt.TLSConfig = &tls.Config{
 			ServerName: serverName,
 			InsecureSkipVerify: true,
 		}
 		client = redis.NewClient(opt)
+		logger.Info("Redis client initialized", zap.Any("opt", opt))
 	}
 
-	
+	logger.Info("Ping redis with following data", 
+		zap.String("ENV", env),
+		zap.String("Server name", serverName),
+		zap.Any("redisURL", redisURL),
+	)
 
-    // opt, err := redis.ParseURL(redisURL)
-	// common.FailOnError(ctx, fmt.Sprintf("failed to parse Redis URL: %w", err), err)
-
-    // opt.TLSConfig = &tls.Config{
-    //     ServerName: serverName,
-    //     InsecureSkipVerify: true,
-    // }
-
-    // client := redis.NewClient(opt)
-	logger.Info("Pinging redis client")
     pong, err := client.Ping(ctx).Result()
 	common.FailOnError(ctx, "Error connecting to Redis", err)
 
     logger.Info("got response from redis client", zap.String("Redis ping response:", pong))
 	return  client, nil
+}
+
+func parseRedisConfig(ctx context.Context) (string, string) {
+	redisURL = os.Getenv("REDIS_URL")
+	if redisURL == "" {
+		common.FailOnError(ctx, "REDIS_URL environment variable is not set", nil)
+	}
+	
+	serverName = os.Getenv("REDIS_TLS_SERVER_NAME")
+	if serverName == "" {
+		common.FailOnError(ctx, "REDIS_TLS_SERVER_NAME is not set", nil)
+	}
+
+	return  redisURL, serverName
 }
