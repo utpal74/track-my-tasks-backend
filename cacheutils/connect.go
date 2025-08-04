2c9f58f38a439815228ae301c4352959779dadea
UTPAL KUMAR <utpal.kumar74@gmail.com>
2025-08-04 17:09:15 +0530

    update in tls config

--- cacheutils/connect.go
+++ cacheutils/connect.go
@@ -5,6 +5,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -13,72 +14,39 @@ import (
 	"go.uber.org/zap"
 )
 
-var (
-	client *redis.Client
-	redisURL, serverName string
-)
-
-// Connect returns a vlid connection with redis instance
 func Connect(ctx context.Context) (*redis.Client, error) {
-	logger := logger.FromCtx(ctx)
-	redisURL, serverName = parseRedisConfig(ctx)
-	
-	// redis client setup (recommended in local development setup)
-	client = redis.NewClient(&redis.Options{
-		Addr: redisURL,
-		DB:   0,
-	})
+	log := logger.FromCtx(ctx)
 
-	// redis client setup overriden in prod
-	if os.Getenv("ENV") == "production" {
-		logger.Info("Attempt redis connection in production mode")
-		opt, err := redis.ParseURL(redisURL)
-		common.FailOnError(ctx, fmt.Sprintf("failed to parse Redis URL: %v", err), err)
-
-		// Only set TLS if using rediss:// -
-		// This is specific to platform hosting redis instance. 
-		// In this case render platform uses internal URL for communication.
-		// More info at https://dashboard.render.com/ 
-		if opt.TLSConfig == nil && opt.Addr != "" && redisURL[:8] == "rediss://" {
-			opt.TLSConfig = &tls.Config{
-				ServerName: serverName,
-				InsecureSkipVerify: true,
-			}
-		}
-
-		client = redis.NewClient(opt)
-		logger.Info("Redis client initialized", 
-			zap.String("Addr", opt.Addr),
-			zap.String("Username", opt.Username),
-			zap.Bool("TLS", opt.TLSConfig != nil),
-		)
+	redisURL := os.Getenv("REDIS_URL")
+	if redisURL == "" {
+		common.FailOnError(ctx, "REDIS_URL is not set", nil)
 	}
 
-	logger.Info("Ping redis --", 
-		zap.String("ENV", os.Getenv("ENV")),
-		zap.String("Server name", serverName),
+	opt, err := redis.ParseURL(redisURL)
+	common.FailOnError(ctx, fmt.Sprintf("failed to parse Redis URL: %v", err), err)
+
+	// TLS only for rediss://
+	if strings.HasPrefix(redisURL, "rediss://") {
+		opt.TLSConfig = &tls.Config{
+			InsecureSkipVerify: true,
+		}
+	}
+
+	client := redis.NewClient(opt)
+
+	log.Info("Redis client initialized",
+		zap.String("Addr", opt.Addr),
+		zap.String("Username", opt.Username),
+		zap.Bool("TLS", opt.TLSConfig != nil),
 	)
 
+	// Test connection
 	pingCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
-    pong, err := client.Ping(pingCtx).Result()
+	pong, err := client.Ping(pingCtx).Result()
 	common.FailOnError(ctx, "Error connecting to Redis", err)
 
-    logger.Info("got response from redis client", zap.String("Redis ping response:", pong))
-	return  client, nil
-}
-
-func parseRedisConfig(ctx context.Context) (string, string) {
-	redisURL = os.Getenv("REDIS_URL")
-	if redisURL == "" {
-		common.FailOnError(ctx, "REDIS_URL environment variable is not set", nil)
-	}
-	
-	serverName = os.Getenv("REDIS_TLS_SERVER_NAME")
-	if serverName == "" {
-		common.FailOnError(ctx, "REDIS_TLS_SERVER_NAME is not set", nil)
-	}
-
-	return  redisURL, serverName
+	log.Info("got response from redis client", zap.String("Redis ping response", pong))
+	return client, nil
 }
